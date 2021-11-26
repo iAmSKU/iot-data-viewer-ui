8bf06bf0dc3aca59345a92f3ea91c735dddf7290
Upadhyay <saurabh.upadhyay@siemens.com>
2021-11-26 21:44:06 +0530

    segregated api end point

--- src/service/IDVApi.js
+++ src/service/IDVApi.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const endPoint = "http://localhost:3001";
+const baseApi = "/api/v1";
 
 export const SaveConfigData = (data) => {
   console.log("save request for:", data);
   axios
-    .post(endPoint + `/api/v1/data`)
+    .post(endPoint + baseApi + `/data`)
     .then(function (response) {
       console.log(response);
     })
